5dee7a604bc86a623502e804b2e650f134badfaf
minjae Kim <hjn966@naver.com>
2023-09-26 17:54:05 +0900

    Change ssd file path

--- testshell.c
+++ testshell.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PROMPT          "testshell >> "
-#define SSD_EXEC_FILE   "../SSD_simulator/ssd"
-#define RESULT_FILE     "./result"
-#define STORAGE_SIZE    100
-#define MAX_INPUT_LEN   32
+#define PROMPT "testshell >> "
+#define SSD_EXEC_FILE "./ssd"
+#define RESULT_FILE "./result"
+#define STORAGE_SIZE 100
+#define MAX_INPUT_LEN 32
 #define MAX_COMMAND_LEN 16
-#define MAX_PARAMS_LEN  16
+#define MAX_PARAMS_LEN 16
 #define MAX_FULLCMD_LEN 32
-#define DATA_LEN        11
+#define DATA_LEN 11
 
-typedef enum mode { NONE, READ, WRITE, FULLREAD, FULLWRITE } command_mode;
+typedef enum mode
+{
+    NONE,
+    READ,
+    WRITE,
+    FULLREAD,
+    FULLWRITE
+} command_mode;
 
 void split(char input[64], char command[16], char params[48]);
 void makeFullCmd(char *fullcmd, char params[48], command_mode c_mode);
@@ -20,13 +27,16 @@ int isValidLBA(char *LBA);
 int isValidValue(char *value);
 command_mode check_command(char *command);
 
-int main() {
-    while (1) {
+int main()
+{
+    while (1)
+    {
         printf(PROMPT);
         char input[MAX_INPUT_LEN] = {0};
         fgets(input, MAX_INPUT_LEN, stdin);
         // too long input
-        if (input[strlen(input) - 1] != '\n' && input[strlen(input) - 1] != 0) {
+        if (input[strlen(input) - 1] != '\n' && input[strlen(input) - 1] != 0)
+        {
             printf("INVALID COMMAND\n");
             while (getchar() != '\n')
                 ;
@@ -34,11 +44,14 @@ int main() {
         }
         input[strlen(input) - 1] = '\0';
         // no input
-        if (strlen(input) == 0) continue;
+        if (strlen(input) == 0)
+            continue;
         // exit
-        if (strcmp(input, "exit") == 0) break;
+        if (strcmp(input, "exit") == 0)
+            break;
         // help
-        if (strcmp(input, "help") == 0) {
+        if (strcmp(input, "help") == 0)
+        {
             system("cat ./help.txt");
             continue;
         }
@@ -53,10 +66,12 @@ int main() {
 
         char LBA[4] = {0}, value[16] = {0};
         int value_type = 0;
-        switch (c_mode) {
+        switch (c_mode)
+        {
         case READ:
             // if command is valid
-            if (isValidLBA(params)) {
+            if (isValidLBA(params))
+            {
                 // render command and execute
                 makeFullCmd(fullcmd, params, READ);
                 system(fullcmd);
@@ -78,7 +93,8 @@ int main() {
             sscanf(params, "%s %s", LBA, value);
 
             // if command is valid
-            if (isValidLBA(LBA) && isValidValue(value)) {
+            if (isValidLBA(LBA) && isValidValue(value))
+            {
                 // render command and execute
                 makeFullCmd(fullcmd, params, WRITE);
                 system(fullcmd);
@@ -90,9 +106,11 @@ int main() {
 
         case FULLREAD:
             // if command is valid (no params)
-            if (strlen(params) == 0) {
+            if (strlen(params) == 0)
+            {
                 // for every LBA
-                for (int i = 0; i < STORAGE_SIZE; i++) {
+                for (int i = 0; i < STORAGE_SIZE; i++)
+                {
                     // render command and execute
                     makeFullCmd(fullcmd, params, READ);
                     system(fullcmd);
@@ -112,9 +130,11 @@ int main() {
         case FULLWRITE:
             // if command is valid
             value_type = isValidValue(params);
-            if (value_type) {
+            if (value_type)
+            {
                 // for every LBA
-                for (int i = 0; i < STORAGE_SIZE; i++) {
+                for (int i = 0; i < STORAGE_SIZE; i++)
+                {
                     // render command and execute
                     makeFullCmd(fullcmd, params, WRITE);
                     system(fullcmd);
@@ -133,11 +153,13 @@ int main() {
     return 0;
 }
 
-void split(char input[64], char command[16], char params[48]) {
+void split(char input[64], char command[16], char params[48])
+{
     sscanf(input, "%s %[^\0]", command, params);
 }
 
-command_mode check_command(char *command) {
+command_mode check_command(char *command)
+{
     if (strcmp(command, "read") == 0)
         return READ;
     else if (strcmp(command, "write") == 0)
@@ -150,55 +172,76 @@ command_mode check_command(char *command) {
         return NONE;
 }
 
-int isValidLBA(char *LBA) {
-    if (strlen(LBA) == 0) {
+int isValidLBA(char *LBA)
+{
+    if (strlen(LBA) == 0)
+    {
         return 0;
     }
-    for (unsigned long i = 0; i < strlen(LBA); i++) {
+    for (unsigned long i = 0; i < strlen(LBA); i++)
+    {
         if ('0' <= LBA[i] && LBA[i] <= '9')
             continue;
-        else {
+        else
+        {
             return 0;
         }
     }
     int num = atoi(LBA);
-    if (num < 0 || num >= STORAGE_SIZE) {
+    if (num < 0 || num >= STORAGE_SIZE)
+    {
         return 0;
     }
     return 1;
 }
 
-int isValidValue(char *value) {
+int isValidValue(char *value)
+{
     // empty
-    if (strlen(value) == 0) return 0;
+    if (strlen(value) == 0)
+        return 0;
 
     // hex
-    if (value[0] == '0' && value[1] == 'x') {
-        if (strlen(value) > 10) return 0;
-        if (value[0] != '0' || value[1] != 'x') return 0;
+    if (value[0] == '0' && value[1] == 'x')
+    {
+        if (strlen(value) > 10)
+            return 0;
+        if (value[0] != '0' || value[1] != 'x')
+            return 0;
 
-        for (unsigned long i = 2; i < strlen(value); i++) {
-            if ((value[i] < 'A' && value[i] > 'F') && (value[i] < '0' && value[i] > '9')) return 0;
+        for (unsigned long i = 2; i < strlen(value); i++)
+        {
+            if ((value[i] < 'A' && value[i] > 'F') && (value[i] < '0' && value[i] > '9'))
+                return 0;
         }
         return 16;
     }
 
     // bin
-    else if (value[0] == 0 && value[1] == 'b') {
-        if (strlen(value) > 34) return 0;
-        for (unsigned long i = 2; i < strlen(value); i++) {
-            if (value[i] != '0' && value[i] != '1') return 0;
+    else if (value[0] == 0 && value[1] == 'b')
+    {
+        if (strlen(value) > 34)
+            return 0;
+        for (unsigned long i = 2; i < strlen(value); i++)
+        {
+            if (value[i] != '0' && value[i] != '1')
+                return 0;
         }
         return 2;
     }
 
     // dec
-    else {
-        if (strlen(value) > 10) return 0;
+    else
+    {
+        if (strlen(value) > 10)
+            return 0;
         unsigned long long num = strtoull(value, NULL, 10);
-        if (num > 0xFFFFFFFF) return 0;
-        for (unsigned long i = 0; i < strlen(value); i++) {
-            if (value[i] < '0' && value[i] > '9') return 0;
+        if (num > 0xFFFFFFFF)
+            return 0;
+        for (unsigned long i = 0; i < strlen(value); i++)
+        {
+            if (value[i] < '0' && value[i] > '9')
+                return 0;
         }
         return 10;
     }
@@ -206,8 +249,10 @@ int isValidValue(char *value) {
     return 0;
 }
 
-void makeFullCmd(char *fullcmd, char params[48], command_mode c_mode) {
-    switch (c_mode) {
+void makeFullCmd(char *fullcmd, char params[48], command_mode c_mode)
+{
+    switch (c_mode)
+    {
     case READ:
         sprintf(fullcmd, "%s R %s", SSD_EXEC_FILE, params);
         break;
